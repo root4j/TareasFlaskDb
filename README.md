d12f3a6625ab4f9d215c5e522d9b31d16c6f279b
Ruthford Jay <ruthford.jay@cannoncol.com>
2020-12-17 20:34:26 -0500

    Aplicacion de Tareas en Flask con Base de Datos SQLite

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # TareasFlaskDb
-Ejemplo desarrollado en Flask para la creación de tareas con bases de datos.
+Ejemplo desarrollado en FlasK para la creación de tareas con bases de datos. Para ejecutar la pagina se debe realizar el siguiernte comando:
+
+```console
+pip install Flask Flask-SQLAlchemy SQLAlchemy
+python app.py
+```
+
+```sql
+CREATE TABLE tarea (
+    id        INTEGER       PRIMARY KEY AUTOINCREMENT
+                            UNIQUE
+                            NOT NULL,
+    nombre    VARCHAR (150) UNIQUE
+                            NOT NULL,
+    realizada BOOLEAN       NOT NULL
+);
+
+CREATE TABLE usuario (
+    email    VARCHAR (150) PRIMARY KEY
+                           UNIQUE
+                           NOT NULL,
+    nombre   VARCHAR (150) UNIQUE
+                           NOT NULL,
+    password VARCHAR (150) 
+);
+
+INSERT INTO usuario VALUES ('admin@tasks.com', 'Administrador del Sistema', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92');
+```
+
+* https://www.python.org/downloads/
+* https://www.w3schools.com/python/default.asp
+* https://flask.palletsprojects.com/en/1.1.x/installation/#installation
+* https://entrenamiento-frameworks-web-python.readthedocs.io/es/latest/leccion6/crud_app.html
+* https://www.youtube.com/channel/UCNdy_LQjD_ew3r5zEXOA0aQ
+* https://www.youtube.com/watch?v=7s1RjItUBqU&list=PLL0TiOXBeDagsYUYFO9WMwDtuDuoGZVB9&ab_channel=Fazt
---
+++ app.py
@@ -0,0 +1,247 @@
+# Importar utilidades del sistema operativo
+import os
+# Importar utilidades de encriptacion
+import hashlib
+
+# De esta manera se importan la librerias que vamos a utilizar
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+# Se obtiene la ruta de la carpeta donde se encuentra el proyecto
+project_dir = os.path.dirname(os.path.abspath(__file__))
+# Se estable la ruta de la base de datos
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "tarea.db"))
+
+# Se crea la instancia de Flask
+app = Flask(__name__)
+# Se le asigna la conexion de la base de datos al contexto Flask
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+# SE inactivan el analisis de cambios
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# Se crea una instancia para manejar la base de datos
+db = SQLAlchemy(app)
+
+# ***********************************
+# * AREA PARA DECLARACION DE CLASES *
+# ***********************************
+# Clase para administrar las tareas
+class Tarea(db.Model):
+    # Propiedades de la tabla
+    __table_args__ = {'sqlite_autoincrement': True}
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(150), unique=True, nullable=False)
+    realizada = db.Column(db.Boolean, nullable=False)
+    # Metodo constructor de la clase
+    def __init__(self, nombre, realizada):
+        self.nombre = nombre
+        self.realizada = realizada
+
+# Clase para administrar usuarios del sistema
+class Usuario(db.Model):
+    # Propiedades de la tabla
+    email = db.Column(db.String(150), nullable=False, primary_key=True)
+    nombre = db.Column(db.String(150), nullable=False)
+    password = db.Column(db.String(150))
+    # Metodo constructor de la clase
+    def __init__(self, email, nombre, password):
+        self.email = email
+        self.nombre = nombre
+        self.password = password
+
+# Clase para administrar mensajes del sistema
+class Mensaje:
+    # Metodo constructor de la clase
+    def __init__(self, css, titulo, texto, esError):
+        self.css = css
+        self.titulo = titulo
+        self.texto = texto
+        self.esError = esError
+
+# **************************************
+# * AREA PARA DECLARACION DE VARIABLES *
+# **************************************
+# Flag de sesion activa (Variable Global)
+sesion = False
+
+# Secuencia de tarea (Variable Global)
+secuencia = 4
+
+# Usuario conectado (Variable Global)
+usuario = None
+
+# Listado de usuarios inicial (Variable Global)
+usuarios = [Usuario("admin@tasks.com", "Administrador del Sistema", "12345"), Usuario("rjay@tasks.com", "Ruthford Jay", "12345")]
+
+# Listado de tareas inicial (Variable Global)
+tareas_list = []
+
+# **************************************
+# * AREA PARA DECLARACION DE FUNCIONES *
+# **************************************
+# +---------------------------------------------+
+# | Funcion que se encarga de la vista de login |
+# +---------------------------------------------+
+@app.route("/", methods=["GET", "POST"])
+def login():
+    # Declaracion para uso de variables globales
+    global sesion
+    global usuario
+    global tareas_list
+    # Si la sesion esta activa se redirige a la vista de tareas
+    if sesion:
+        return render_template("tareas.html", usuario=usuario, tareas=tareas_list)
+    mensaje = Mensaje("alert-info", "", "", False)
+    # Se verifica que verbo HTTP se utilizo para la peticion. Si es POST es el formulario, si es GET es desde la URL
+    if request.method == "GET":
+        return render_template("login.html", mensaje=mensaje)
+    else:
+        # Se obtienen los datos del formulario
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        # Se encripta el password para compararlo con el de la base de datos
+        criptoPassword = hashlib.sha256(str(password).encode('utf-8')).hexdigest()
+
+        # Se busca si el usuario se encuentra registrado y su contraseña esta correcta
+        usuario = Usuario.query.filter_by(email=email, password=criptoPassword).first()
+
+        # Validacion del usuario
+        if usuario != None:
+            # Se establece la variable sesion en verdadero
+            sesion = True
+            # Se consultan todas las tareas en la base de datos
+            tareas = Tarea.query.all()
+            return render_template("tareas.html", usuario=usuario, tareas=tareas)
+        else:
+            mensaje = Mensaje("alert-danger", "Validación", "Correo Electronico y/o Contraseña estan errados.", True)
+            return render_template("login.html", mensaje=mensaje)
+
+# +----------------------------------------------+
+# | Funcion que se encarga de la vista de tareas |
+# +----------------------------------------------+
+@app.route("/tareas")
+def tareas():
+    # Declaracion para uso de variables globales
+    global sesion
+    global usuario
+    # Si la sesion esta iactiva se redirige a la vista de tareas
+    if sesion:
+        # Se consultan todas las tareas en la base de datos
+        tareas = Tarea.query.all()
+        return render_template("tareas.html", usuario=usuario, tareas=tareas)
+    else:
+        mensaje = Mensaje("alert-info", "Sin Sesion", "Sesion Expirada", True)
+        return render_template("login.html", mensaje=mensaje)
+
+# +--------------------------------------------+
+# | Funcion que se encarga de cerrar la sesion |
+# +--------------------------------------------+
+@app.route("/salir")
+def salir():
+    # Declaracion para uso de variables globales
+    global sesion
+    global usuario
+    sesion = False
+    usuario = None
+    mensaje = Mensaje("alert-info", "Sin Sesion", "Sesion Terminada", True)
+    return render_template("login.html", mensaje=mensaje)
+
+# +----------------------------------------+
+# | Funcion que se encarga de crear tareas |
+# +----------------------------------------+
+@app.route("/crear", methods=["GET", "POST"])
+def crear():
+    # Declaracion para uso de variables globales
+    global usuario
+    global sesion
+
+    mensaje = Mensaje("alert-info", "", "", False)
+
+    # Se verifica la sesion, sino esta activa se redirige al login
+    if not sesion:
+        mensaje = Mensaje("alert-info", "Sin Sesion", "Sesion Expirada", True)
+        return render_template("login.html", mensaje=mensaje)
+
+    # Se verifica que verbo HTTP se utilizo para la peticion. Si es POST es el formulario, si es GET es desde la URL
+    if request.method == "GET":
+        return render_template("crear.html", usuario=usuario, mensaje=mensaje)
+    else:
+        # Se obtienen los datos del formulario
+        nombre = request.form.get("nombre")
+
+        # Se inserta la nueva tarea
+        try:
+            tarea = Tarea(nombre, False)
+            db.session.add(tarea)
+            db.session.commit()
+        except Exception as e:
+            print(e)
+
+        # Se consultan todas las tareas en la base de datos
+        tareas = Tarea.query.all()
+        return render_template("tareas.html", usuario=usuario, tareas=tareas)
+
+# +-------------------------------------------+
+# | Funcion que se encarga de eliminar tareas |
+# +-------------------------------------------+
+@app.route("/tarea/d/<int:id>")
+def eliminar(id):
+    # Declaracion para uso de variables globales
+    global usuario
+    global sesion
+
+    mensaje = Mensaje("alert-info", "", "", False)
+
+    # Se verifica la sesion, sino esta activa se redirige al login
+    if not sesion:
+        mensaje = Mensaje("alert-info", "Sin Sesion", "Sesion Expirada", True)
+        return render_template("login.html", mensaje=mensaje)
+
+    # Se busca si el usuario se encuentra registrado y su contraseña esta correcta
+    tarea = Tarea.query.filter_by(id=id).first()
+    if tarea != None:
+        # Se elimina la tarea
+        try:
+            db.session.delete(tarea)
+            db.session.commit()
+        except Exception as e:
+            print(e)
+
+    # Se consultan todas las tareas en la base de datos
+    tareas = Tarea.query.all()
+    return render_template("tareas.html", usuario=usuario, tareas=tareas)
+
+# +---------------------------------------------+
+# | Funcion que se encarga de actualizar tareas |
+# +---------------------------------------------+
+@app.route("/tarea/u/<int:id>")
+def actualizar(id):
+    # Declaracion para uso de variables globales
+    global usuario
+    global sesion
+
+    mensaje = Mensaje("alert-info", "", "", False)
+
+    # Se verifica la sesion, sino esta activa se redirige al login
+    if not sesion:
+        mensaje = Mensaje("alert-info", "Sin Sesion", "Sesion Expirada", True)
+        return render_template("login.html", mensaje=mensaje)
+
+    # Se busca si el usuario se encuentra registrado y su contraseña esta correcta
+    tarea = Tarea.query.filter_by(id=id).first()
+    if tarea != None:
+        # Se elimina la tarea
+        try:
+            tarea.realizada = not tarea.realizada
+            db.session.commit()
+        except Exception as e:
+            print(e)
+
+    # Se consultan todas las tareas en la base de datos
+    tareas = Tarea.query.all()
+    return render_template("tareas.html", usuario=usuario, tareas=tareas)
+
+# Activar el modo debug de la aplicacion
+if __name__ == "__main__":
+    app.run(debug=True)
